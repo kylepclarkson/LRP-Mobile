f42d1562cb4c1284a00a28b09b8a0a1d34627fef
Kyle Clarkson <kyleclarkson17@hotmail.com>
2025-10-25 21:40:40 -0700

    Create UserContext placeholder.

---
+++ lib/context/user.tsx
@@ -0,0 +1,32 @@
+import { AuthenticatedUser } from '@/types/users/User';
+import React, { createContext, useContext, useState } from 'react';
+
+
+type UserContextType = {
+  user: AuthenticatedUser | null;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export function UserProvider(
+  { children }: { children: React.ReactNode }
+) {
+  // TODO move user functionality from existing context to this one. 
+  // An authenticated user. Will be null if user is not authenticated
+  const [user, setUser] = React.useState<AuthenticatedUser | null>(null);
+  return (
+    <UserContext.Provider value={{
+      user
+    }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export function useUserSession() {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("context must be defined within UserProvider");
+  }
+  return context;
+}
