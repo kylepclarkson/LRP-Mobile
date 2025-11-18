ca9ad83c47b0fada848aecb0e3937abc63c5ba6d
Kyle Clarkson <kyleclarkson17@hotmail.com>
2025-11-17 22:32:37 -0800

    Installed Nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "./global.css";
 
 function InitialLayout() {
 
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const { withStorybook } = require("@storybook/react-native/metro/withStorybook");
+const { withNativewind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 /** withStorybook Adds the config that storybook uses */
-module.exports = withStorybook(config);
+module.exports = withNativewind(withStorybook(config));
