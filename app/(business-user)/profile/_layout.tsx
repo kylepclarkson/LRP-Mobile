c8248fea14e1cbd3c3bbe6f18996d0e78635c631
Kyle Clarkson <kyleclarkson17@hotmail.com>
2026-01-08 19:28:21 -0800

    Set BusinessUserProfile's header text.

--- app/(business-user)/profile/_layout.tsx
+++ app/(business-user)/profile/_layout.tsx
@@ -1,9 +1,15 @@
+import { PageHeader } from "@/components/common/PageHeader";
 import { Stack } from "expo-router";
 
 export default function BusinessUserProfileLayout() {
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          header: () => <PageHeader headerText={`Profile - Business View`} />
+        }} />
     </Stack>
   );
 }
