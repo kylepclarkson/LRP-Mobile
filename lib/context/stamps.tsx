29a743df72a06978ace49070eefb6ea57c5ecb0d
Kyle Clarkson <kyleclarkson17@hotmail.com>
2026-02-04 08:44:27 -0800

    Started api/stamps module.

--- lib/context/stamps.tsx
+++ lib/context/stamps.tsx
@@ -1,5 +1,5 @@
 import { StampCard } from "@/types/stamps";
-import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
+import React, { createContext, useCallback, useContext, useEffect, useRef, useState } from "react";
 import { getStampCards } from "../services/stamps.service";
 import { useAuthContext } from "./auth";
 
@@ -19,14 +19,41 @@ export function StampsProvider(
   { children }: { children: React.ReactNode }
 ) {
   const { user } = useAuthContext();
+  // Provide a reference to the user to avoid stale closure issues. 
+  const userRef = useRef(user);
 
   // Stamp card states
   const [stampCards, setStampCards] = useState<StampCard[]>([]);
   const [loadingStampCards, setLoadingStampCards] = useState<boolean>(false);
   const [errorStampCards, setErrorStampCards] = useState<string | null>(null);
 
-  // Stamp rewards states - TODO
 
+  const refreshStampCards = useCallback(async (queryParams?: string) => {
+    if (!userRef.current) {
+      console.debug("Clearing stamp cards");
+      setStampCards([]);
+      return;
+    }
+    try {
+      setLoadingStampCards(true);
+      // TODO implement and user stamps.service.ts w/ query params
+      const data = await getStampCards(queryParams);
+      setStampCards(data);
+      console.debug("Fetched stamp cards");
+    } catch (err) {
+      console.error("Error fetching stamp cards", err);
+      setErrorStampCards("Error fetching stamp cards");
+    } finally {
+      setLoadingStampCards(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    refreshStampCards();
+  }, [user]);
+
+
+  // --- TODO replace below with the above.
   const fetchStampCards = useCallback(async (queryParams?: string) => {
     setLoadingStampCards(true);
     try {
@@ -50,6 +77,7 @@ export function StampsProvider(
     fetchStampCards();
 
   }, [user, fetchStampCards]);
+  // --- ---
 
   return (
     <StampsContext.Provider value={{
