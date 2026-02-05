aa7236035b61bb98b3bdcf4a23d868f64848f571
Kyle Clarkson <kyleclarkson17@hotmail.com>
2026-02-04 22:12:28 -0800

    Created custom homepage header. Refactored stamp list.

--- app/(customer-user)/home/_layout.tsx
+++ app/(customer-user)/home/_layout.tsx
@@ -1,5 +1,30 @@
-import { PageHeader } from "@/components/common/PageHeader";
+import { Image } from 'expo-image';
 import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+const logo = require('@/assets/images/aandeg-icon.png');
+
+function HomePageHeader() {
+  return (
+    <View className="h-20 flex-row items-center justify-center px-4 py-1 bg-primary">
+      {/* Left-aligned logo that auto-scales to header height */}
+      <View className="absolute left-6 h-full justify-center">
+        <View className='rounded-full bg-primary-content'>
+          <Image
+            source={logo}
+            style={styles.logo}
+            className="h-full w-auto"
+            contentFit="contain"
+          />
+        </View>
+      </View>
+
+      {/* Centered title */}
+      <Text className="text-xl font-semibold text-primary-content">Aandeg</Text>
+    </View>
+  );
+}
+
 
 export default function HomeLayout() {
   return (
@@ -7,10 +32,19 @@ export default function HomeLayout() {
       <Stack.Screen
         name="index"
         options={{
-          header: () => <PageHeader headerText="Home" />
+          header: () => <HomePageHeader />
         }}
       />
       <Stack.Screen name="business" options={{ headerShown: false }} />
     </Stack>
   );
 }
+
+// Note: Expo image does not support classname prop for styling so stylesheet is used. 
+const styles = StyleSheet.create({
+  logo: {
+    height: "100%",
+    width: undefined,
+    aspectRatio: 1,
+  },
+});
--- app/(customer-user)/tracker/index.tsx
+++ app/(customer-user)/tracker/index.tsx
@@ -6,15 +6,27 @@ import { router } from "expo-router";
 import { useEffect } from "react";
 import { FlatList, Pressable, Text, View } from "react-native";
 
+function StampListHeader() {
+  return (
+    <View className="ml-2 py-4">
+      <Text className="text-2xl font-bold mb-1">Your Stamp Cards</Text>
+    </View>
+  )
+}
+
+/**
+ * Displays the user's stamp cards. 
+ * By default the stamp cards are filter to be those "in-progress".
+ */
 export default function RewardTrackerScreen() {
 
   const { stampCards, loadingStampCards, refreshStampCards } = useStampsContext();
-  // filter for cards that are in progress
+  // TODO - implement filter for cards that are in progress
   const inProgressStampCards = stampCards.filter(card => card.state === StampCardState.IN_PROGRESS)
 
   useEffect(() => {
     refreshStampCards();
-  }, [])
+  }, []);
 
   const renderStampCardItem = ({ item }: { item: StampCard }) => (
     <Pressable
@@ -38,18 +50,36 @@ export default function RewardTrackerScreen() {
   );
 
   return (
-    <View className="pt-6 flex-1 bg-white">
-      <FlatList
-        data={inProgressStampCards}
-        keyExtractor={(item) => item.id}
-        renderItem={renderStampCardItem}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 40 }}
-        // ListHeaderComponent={<PageHeader headerText="Your Stamp Cards" />}
-        stickyHeaderIndices={[0]}
+    <View className="flex-1">
+      {/* Loading state */}
+      {loadingStampCards && (
+        <View className="mt-10 items-center">
+          <LoadingOverlay />
+        </View>
+      )}
 
-        ListEmptyComponent={loadingStampCards ? <LoadingOverlay /> : null}
-      />
+      {/* Empty state */}
+      {!loadingStampCards && stampCards.length === 0 && (
+        <View className="mt-10 items-center">
+          <Text className="text-lg text-gray-900">No stamp cards yet</Text>
+        </View>
+      )}
+
+      {/* List */}
+      {!loadingStampCards && stampCards.length > 0 && (
+        <View className="flex-1 px-4 pt-6">
+          <FlatList
+            data={stampCards}
+            keyExtractor={(item) => item.id}
+            refreshing={loadingStampCards}
+            ListHeaderComponent={StampListHeader}
+            onRefresh={refreshStampCards}
+            renderItem={renderStampCardItem}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 40 }}
+          />
+        </View>
+      )}
     </View>
 
   );
