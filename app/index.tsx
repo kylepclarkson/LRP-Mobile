781bb9a90e388a11c125f3efbf92cec5ece333cc
Kyle Clarkson <kyleclarkson17@hotmail.com>
2026-02-05 17:57:21 -0800

    Added aandeg logo to login.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import LoginForm from "@/components/forms/login";
+import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import {
   KeyboardAvoidingView,
@@ -20,7 +21,14 @@ export default function Index() {
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       className="flex-1"
     >
-      <View className="flex-1 justify-center p-4">
+      <View className="flex-1 justify-center p-4 bg-primary">
+        <View className="rounded-full bg-primary-content h-30 w-30 self-center p-1 mb-2">
+          <Image
+            source={require('@/assets/images/aandeg-icon.png')}
+            contentFit="contain"
+            style={{ height: '100%' }}
+          />
+        </View>
         <Text className="text-center mb-5 text-surface-foreground text-xl font-semibold">
           Welcome to Aandeg
         </Text>
